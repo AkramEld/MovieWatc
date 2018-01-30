1998f922ec8f7d317790eb0e47a2696f09ad2d04
Akram Eldamaty <33161840+AkramEld@users.noreply.github.com>
2018-01-29 20:31:54 -0500

    Added Files

---
+++ main.js
@@ -0,0 +1,106 @@
+//example use of the api
+
+function apiCall(){
+  x=document.getElementById("form1");
+  show=x.elements["tvshow"].value;
+  season=x.elements["seasons"].value;
+
+
+
+  $.getJSON('https://www.omdbapi.com/?apikey=c4fc25b5&t=' + encodeURI(show)).then(function(response){
+    if(response.Response=="False"){
+
+      window.alert("Please enter a correct TV Show");
+
+  //  $('button').attr('data-toggle', "modal");
+  //  $('button').attr('data-target', "#exampleModal");
+      console.log(response.Response);
+       var response = response.Response;
+
+         //document.getElementById('addButton').data-toggle="modal";
+         //document.getElementById('addButton').data-target="#exampleModal";
+
+
+         //$('button').attr('data-toggle', "");
+        // $('button').attr('data-target', "");
+         //data-toggle="modal" data-target="#exampleModal"
+
+    }
+
+    if(response.Response=="True"){
+
+
+  document.getElementById("list").innerHTML+="<font color=white>"+"<b>"+show+"</b>" + " "  + "     "+ "(" + season + ")" +"<br>"+"</font>";
+
+  var LengthOfEpisodeString = (response.Runtime).replace(' min','');; //inshallah this works lol damn this is smart
+  var LengthOfEpisode = parseInt(LengthOfEpisodeString); //oktry it
+
+
+  asignVariable(0,LengthOfEpisode);
+
+  var image = response.Poster;
+  if (image != "N/A"){
+    document.getElementById('pic').src=image;
+    document.getElementById('pic').style="border-radius:5%"
+  }
+}
+
+
+
+
+  })
+
+  var numEp=0;
+  var numberofep=0;
+  var counter=1;
+  for(i=1; i<=season;i++){
+
+
+    $.getJSON('https://www.omdbapi.com/?apikey=c4fc25b5&t=' + encodeURI(show) +"&season="+i ).then(function(response){
+
+      if(response.Response=="True"){
+      numEp+=response.Episodes.length;
+
+
+
+      if(season==counter){
+        asignVariable(numEp,0);
+      }
+
+      counter++;
+}
+    })
+  }
+
+}
+
+var time=0;
+var numEp=0;
+var total=0;
+
+function asignVariable(data,runtime){
+
+
+  numEp+= data;
+  time+=runtime;
+
+  if(numEp!=0 && time!=0){
+
+    var timeForShow = time * numEp;
+    var daysShow = parseInt(timeForShow/24/60);
+    var hoursShow = parseInt(timeForShow/60%24);
+    var showTimeString = "<font color=white>"+"       " + String(daysShow) + " days : " + String(hoursShow) + " hours : " + String(timeForShow%60) + " minutes"+"</font>";
+    document.getElementById("seriesmin").innerHTML+=showTimeString+" <br>";
+
+    total+=numEp*time;
+    var days = parseInt(total/24/60);
+    var hours = parseInt(total/60%24);
+    var totalString ="<font color=white>" +String(days) + " days : " + String(hours) + " hours : " + String(total%60) + " minutes"+"</font>";
+    document.getElementById("total").innerHTML="<font color=white>"+"Total = " + totalString+"</font>";
+
+
+
+    numEp=0;
+    time=0;
+  }
+}
